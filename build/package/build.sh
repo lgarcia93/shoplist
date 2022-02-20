4c6cf829dd89e3349a5acaf9b6c7f3a1750fcc5d
Lucas Garcia <garciamgo@gmail.com>
2022-02-20 16:40:21 -0300

    Modifying app default port

--- build/package/build.sh
+++ build/package/build.sh
@@ -4,4 +4,4 @@ git pull origin master
 
 sudo docker rm -f docker-shoplist:latest
 
-sudo docker build --build-arg app_env=prod --build-arg app_host_port=3000 --no-cache --tag docker-shoplist:latest -f build/package/Dockerfile .
+sudo docker build --build-arg app_env=prod --build-arg app_port=5000 --no-cache --tag docker-shoplist:latest -f build/package/Dockerfile .
